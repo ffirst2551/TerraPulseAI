a73117c07d728fca9b6622ec9dc22e4af4205349
Skibidi Boy <168410745+ffirst2551@users.noreply.github.com>
2026-03-03 01:04:58 +0700

    Delete TerraPulse AI directory

--- TerraPulse AI/backend/src/lib/redis.js
+++
@@ -1,9 +0,0 @@
-import IORedis from "ioredis";
-import { env } from "../config/env.js";
-
-export const redisConnection = env.USE_MOCK_DATA
-  ? null
-  : new IORedis(env.REDIS_URL, {
-      maxRetriesPerRequest: null,
-      enableReadyCheck: true,
-    });
